63a7cd1311ffd18bed9166d81b9a5b04c8e4a175
Yuri <fyurivs@outlook.com>
2023-08-21 17:36:27 -0300

    finalização do modulo de auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import { UserPayload } from './interfaces/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './interfaces/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
+  login(user: User): UserToken {
+    // throw new Error('Method not implemented.');
+    const payload: UserPayload = {
+      sub: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const token = this.jwtService.sign(payload);
+    return {
+      access_token: token,
+    };
+  }
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
 
