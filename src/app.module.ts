8c7fef9300741c6ebe3e60cba7deca467dfdc2e4
Yuri <fyurivs@outlook.com>
2023-08-21 18:14:08 -0300

    feat/auth: jwt validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,25 @@ import { StoresModule } from './stores/stores.module';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 
 @Module({
   imports: [ClientsModule, StoresModule, CommonModule, UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply((req: any, res: any, next: any) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Substitua pela origem permitida
+        res.header('Access-Control-Allow-Origin', '*'); // Substitua pela origem permitida
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header(
           'Access-Control-Allow-Headers',
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,13 @@ import {
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './interfaces/AuthRequest';
+import { isPublic } from './decorators/is-public.decorator';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @isPublic()
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
